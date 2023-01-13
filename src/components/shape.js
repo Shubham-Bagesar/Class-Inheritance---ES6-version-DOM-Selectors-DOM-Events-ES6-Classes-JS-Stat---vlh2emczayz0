319e7a906c0f717857f90e5e4f6896501ed12b24
shubham bagesar <110713826+Shubham-Bagesar@users.noreply.github.com>
2023-01-14 00:29:25 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,9 @@
-// declare class
-
+class Shape {
+  constructor() {
+    this.color = "green";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
 // export class using module.exports
+module.exports = Shape;
